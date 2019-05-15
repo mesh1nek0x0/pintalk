fb370763755f5eed8a32bae3a94971687e3f67e0
mesh1nek0x0 <mesh1nek0x0@gmail.com>
2019-05-15 22:28:30 +0900

    :wrench: show link list

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,6 +1,11 @@
 import React, { FC } from 'react';
+import { Link } from 'react-router-dom';
+import { Header, List } from 'semantic-ui-react';
 import MyHeader from '../../components/MyHeader';
 import MyFooter from '../../components/MyFooter';
+import { NavigationLinkData } from './LinkList';
+
+const codes = Object.keys(NavigationLinkData);
 
 const Home: FC = () => {
     return (
@@ -9,6 +14,14 @@ const Home: FC = () => {
             <div className="Home">
                 Home Component
             </div>
+            <Header as="h2" content="Navigation Links"/>
+            <List as="ul">
+                {codes.map(code => (
+                    <List.Item as="li" key={code}>
+                        <Link to={NavigationLinkData[code].url}>{NavigationLinkData[code].name}</Link>
+                    </List.Item>
+                ))}
+            </List>
             <MyFooter />
         </>
     );
